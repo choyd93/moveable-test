0e940a74690aa483e77d8831b4cadccfce5e8ae8
youngdong <choyd93@naver.com>
2024-09-12 15:05:11 +0900

    chore: vuetify 설정 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  app: {
-    head: {
-      script: [
-        {
-          src: "//daybrush.com/moveable/release/latest/dist/moveable.min.js",
-          defer: true, // 스크립트가 로드된 후 실행되도록 설정
-        },
-      ],
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    app: {
+        head: {
+            script: [
+                {
+                    src: '//daybrush.com/moveable/release/latest/dist/moveable.min.js',
+                    defer: true // 스크립트가 로드된 후 실행되도록 설정
+                }
+            ]
+        }
     },
-  },
+    modules: ['vuetify-nuxt-module'],
+    vuetify: {
+        moduleOptions: {
+            /* module specific options */
+        },
+        vuetifyOptions: {
+            /* vuetify options */
+            icons: {
+                defaultSet: 'mdi',
+                sets: ['mdi', 'fa']
+            }
+        }
+    }
 });
